8c906df7db8c6e43b5cce5916896efb91aba98f6
tommartensen <mail@tommartensen.de>
2018-11-15 10:19:24 +0100

    format output of cpu and memory test

--- assignment1/memsweep.sh
+++ assignment1/memsweep.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 EXECUTABLE="memsweep"
 if [ ! -e $EXECUTABLE ] ; then
-	echo "Compiling memsweep.c"
 	cc -O -o memsweep memsweep.c -lm
 fi
 
-echo "Running memsweep benchmark"
 if [ "$SYSTEMROOT" = "C:\Windows" ] ; then
 	./memsweep.exe
 else
