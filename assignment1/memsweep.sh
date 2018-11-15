f0e5fdf5d91216883d122ba5fb0f6d7a05fd769a
tommartensen <mail@tommartensen.de>
2018-11-15 18:48:07 +0100

    memsweep loop and aggregation

--- assignment1/memsweep.sh
+++ assignment1/memsweep.sh
@@ -4,8 +4,14 @@ if [ ! -e $EXECUTABLE ] ; then
 	cc -O -o memsweep memsweep.c -lm
 fi
 
-if [ "$SYSTEMROOT" = "C:\Windows" ] ; then
-	./memsweep.exe
-else
-	./memsweep
-fi
+SUM="0.0"
+NUM="0"
+
+while [ $NUM -lt 2 ] ; do
+  # Get score from executable, grep only score not the noisy output!
+  SCORE=$(./$EXECUTABLE  | grep -o -P '\d+\.\d{3}')
+  NUM=$(($NUM + 1))
+  SUM=`echo $SUM+$SCORE | bc`
+done
+# Calculate average, three decimal places should be enough
+echo $(echo "scale=3; $SUM/$NUM" | bc -l)
