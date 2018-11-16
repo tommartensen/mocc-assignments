598c236ef345af17c324806aaf0da17760e90e17
Tom Martensen <mail@tommartensen.de>
2018-11-16 17:23:06 +0100

    Update measure-cpu.sh

--- assignment1/measure-cpu.sh
+++ assignment1/measure-cpu.sh
@@ -4,9 +4,4 @@ if [ ! -e $EXECUTABLE ] ; then
 	cc -O -o linpack linpack.c -lm
 fi
 
-if [ "$SYSTEMROOT" = "C:\Windows" ] ; then
-	result=$(./linpack.exe | tail -1 | sed "s/[[:blank:]]\+/ /g" | cut -d " " -f 7)
-else
-	result=$(./${EXECUTABLE} | tail -1 | sed "s/[[:blank:]]\+/ /g" | cut -d " " -f 7)
-fi
-echo $result
+echo $(./${EXECUTABLE} | tail -1 | sed "s/[[:blank:]]\+/ /g" | cut -d " " -f 7)
