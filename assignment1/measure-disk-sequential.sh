7169d5e156f5fa3e0d6332af0f5c5db053b7c179
Tom Martensen <mail@tommartensen.de>
2018-11-16 17:32:58 +0100

    average write and read rate

--- assignment1/measure-disk-sequential.sh
+++ assignment1/measure-disk-sequential.sh
@@ -5,20 +5,20 @@ print_rate() {
     BYTES_COPIED=$1
     TIME_ELAPSED=$(echo $8 | sed 's/,/\./')
     # Calculate rate in Bytes/second
-    echo $(echo "scale=3; $BYTES_COPIED/$TIME_ELAPSED" | bc -l)    
+    echo $(echo "scale=3; $BYTES_COPIED/$TIME_ELAPSED" | bc -l)
 }
 
 
 # Write speed, copy from /dev/zero to special file the specified amount of data (should fit into RAM completely), get last line of output (had to redirect stderr to stdout for this)
 FILE_SIZE="256M"
 OUTPUT=$(dd if=/dev/zero of=/tmp/test1.img bs=$FILE_SIZE count=1 oflag=dsync 2>&1 | tail -n 1)
-
-print_rate $OUTPUT
+WRITE_RATE=$(print_rate $OUTPUT)
 
 # Test read speed
 # Flush and drop caches before starting
 sh -c "sync && echo 3 > /proc/sys/vm/drop_caches"
 # Read file written beforehand to /dev/null with block size 8k
 OUTPUT=$(dd if=/tmp/test1.img of=/dev/null bs=8k 2>&1 | tail -n 1)
+READ_RATE=$(print_rate $OUTPUT)
 
-print_rate $OUTPUT
+echo "scale=3; ($WRITE_RATE+$READ_RATE)/2" | bc -l
