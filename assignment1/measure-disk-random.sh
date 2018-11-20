5b41e62326d463fe6b8ab568758439ccede261e9
Fabian Engel <fabian.engel@student.hpi.uni-potsdam.de>
2018-11-20 11:43:56 +0100

    Include review comments.

--- assignment1/measure-disk-random.sh
+++ assignment1/measure-disk-random.sh
@@ -9,6 +9,8 @@ RUNTIME=1
 #Test read and write operations for random workload with deactivated page cache and reduced gettimeofday() calls
 #For Ubuntu
 #--ioengine=libaio
+#For MacOS
+#--ioengine=posixaio
 RWFIO=$(fio --randrepeat=1 --direct=1 --runtime=$RUNTIME --ioengine=posixaio \
 --gtod_reduce=1 --name=test --filename=test --bs=$BLOCKSIZE --iodepth=$IODEPTH --size=$SIZE --readwrite=randrw)
 
@@ -18,8 +20,8 @@ RESULTFIO=$(echo "$RWFIO" | grep 'iops' | cut -d ',' -f3 | sed 's/avg=//')
 READS=$(echo "$RESULTFIO" | head -n 1)
 WRITES=$(echo "$RESULTFIO" | tail -n 1)
 
-echo "Read operations per second: $READS" 
-echo "Write operations per second: $WRITES" 
+#echo "Read operations per second: $READS" 
+#echo "Write operations per second: $WRITES" 
 
 #Add the read and write iops
 RESULTSUM=$(echo "$RESULTFIO" | paste -sd+ -)
