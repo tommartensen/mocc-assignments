97219eb1fd0bda1de49653a80557509e5ce87d28
tommartensen <mail@tommartensen.de>
2018-11-21 12:03:19 +0100

    fix random disk measurement

--- assignment1/measure-disk-random.sh
+++ assignment1/measure-disk-random.sh
@@ -2,17 +2,17 @@
 
 #Number of parallel requests
 IODEPTH=16
-SIZE='4M'
-BLOCKSIZE='4B'
-RUNTIME=1
+SIZE='1g'
+BLOCKSIZE='4k'
+RUNTIME=20
 
 #Test read and write operations for random workload with deactivated page cache and reduced gettimeofday() calls
 #For Ubuntu
 #--ioengine=libaio
 #For MacOS
 #--ioengine=posixaio
-RWFIO=$(fio --randrepeat=1 --direct=1 --runtime=$RUNTIME --ioengine=posixaio \
---gtod_reduce=1 --name=test --filename=test --bs=$BLOCKSIZE --iodepth=$IODEPTH --size=$SIZE --readwrite=randrw)
+
+RWFIO=$(fio --randrepeat=1 --direct=1 --runtime=$RUNTIME --ioengine=libaio --gtod_reduce=1 --name=test --filename=/tmp/test --bs=$BLOCKSIZE --iodepth=64 --size=$SIZE --readwrite=randrw)
 
 #Extract read and write values
 RESULTFIO=$(echo "$RWFIO" | grep 'iops' | cut -d ',' -f3 | sed 's/avg=//')
