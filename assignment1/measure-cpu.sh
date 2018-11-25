42c2b9e7c56557609282ec48711c9e14e052e035
tommartensen <mail@tommartensen.de>
2018-11-25 12:46:37 +0100

    revert changes to scripts that were due to cronjob

--- assignment1/measure-cpu.sh
+++ assignment1/measure-cpu.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 EXECUTABLE="linpack"
 if [ ! -e $EXECUTABLE ] ; then
-	cc -O -o $(dirname $0)/linpack $(dirname $0)/linpack.c -lm
+	#cc -O -o $(dirname $0)/linpack $(dirname $0)/linpack.c -lm -> used during cronjob, due to absolute paths
+	cc -O -o linpack linpack.c -lm
 fi
 
-echo $(/$(dirname $0)/${EXECUTABLE} | tail -1 | sed "s/[[:blank:]]\+/ /g" | cut -d " " -f 7)
+#echo $(/$(dirname $0)/${EXECUTABLE} | tail -1 | sed "s/[[:blank:]]\+/ /g" | cut -d " " -f 7) -> used during cronjob, due to absolute paths
+echo $(./${EXECUTABLE} | tail -1 | sed "s/[[:blank:]]\+/ /g" | cut -d " " -f 7)
--- assignment1/measure-mem.sh
+++ assignment1/measure-mem.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 EXECUTABLE="memsweep"
 if [ ! -e $EXECUTABLE ] ; then
-  cc -O -o $(dirname $0)/memsweep $(dirname $0)/memsweep.c -lm
+  cc -O -o memsweep memsweep.c -lm
+  #cc -O -o $(dirname $0)/memsweep $(dirname $0)/memsweep.c -lm -> used during cronjob, because of absolute path
 fi
 
 SUM="0.0"
 NUM="0"
 
-# Get score from executable, grep only score, not the noisy output!
-SCORE=$(/$(dirname $0)/$EXECUTABLE  | grep -o -P '\d+\.\d{3}')
-NUM=$(($NUM + 1))
-SUM=`echo $SUM + $SCORE | bc`
+while [ $NUM -lt 2 ]; do
+  # Get score from executable, grep only score, not the noisy output!
+  SCORE=$(./$EXECUTABLE  | grep -o -P '\d+\.\d{3}')
+  # SCORE=$(/$(dirname $0)/$EXECUTABLE  | grep -o -P '\d+\.\d{3}') -> used during cronjob, because of absolute path
+  NUM=$(($NUM + 1))
+  SUM=`echo $SUM + $SCORE | bc`
+done
+
 # Calculate average, three decimal places should be enough
-echo $(echo "scale=3; $SUM/$NUM" | bc -l)
+echo "scale=3; $SUM/$NUM" | bc -l
