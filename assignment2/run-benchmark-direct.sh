89f642ccf1392bfa5da0ecb120db3ae80aa223fa
tommartensen <mail@tommartensen.de>
2018-12-10 17:31:40 +0100

    build benchmark runners

---
+++ assignment2/run-benchmark-direct.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BENCHMARK_TYPE=$1
+
+echo "time,value" > $BENCHMARK_TYPE-cpu.csv
+echo "time,value" > $BENCHMARK_TYPE-mem.csv
+echo "time,value" > $BENCHMARK_TYPE-disk-random.csv
+echo "time,value" > $BENCHMARK_TYPE-fork.csv
+echo "time,value" > $BENCHMARK_TYPE-nginx.csv
+
+for i in `seq 1 48`; do
+  echo "$(date +%s),$(sh measure-cpu.sh)" >> $BENCHMARK_TYPE-cpu.csv
+  echo "$(date +%s),$(sh measure-mem.sh)" >> $BENCHMARK_TYPE-mem.csv
+  echo "$(date +%s),$(sh measure-disk-random.sh)" >> $BENCHMARK_TYPE-disk-random.csv
+  echo "$(date +%s),$(sh measure-fork.sh)" >> $BENCHMARK_TYPE-fork.csv
+  echo "$(date +%s),$(sh measure-nginx.sh)" >> $BENCHMARK_TYPE-nginx.csv
+  # Flush caches?
+done
---
+++ assignment2/run-benchmark-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "time,value" > docker-cpu.csv
+echo "time,value" > docker-mem.csv
+echo "time,value" > docker-disk-random.csv
+echo "time,value" > docker-fork.csv
+echo "time,value" > docker-nginx.csv
+
+for i in `seq 1 48`; do
+  echo "$(date +%s),$(docker run measure-cpu)" >> docker-cpu.csv
+  echo "$(date +%s),$(docker run measure-mem)" >> docker-mem.csv
+  echo "$(date +%s),$(docker run measure-disk-random)" >> docker-disk-random.csv
+  echo "$(date +%s),$(docker run measure-fork)" >> docker-fork.csv
+  echo "$(date +%s),$(docker run measure-nginx)" >> docker-nginx.csv
+done
